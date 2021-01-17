9f8a5f74b38aaf53b8b324544ea178eae5ccf68a
MHMDHIDR <Mr.hamood277@gmail.com>
2021-01-17 23:16:53 +0300

    added animation onload to header

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,27 +11,36 @@ const form = {
   submitBtn: document.getElementById('submitBtn'),
   formMassages: document.querySelector('.form-group:last-of-type')
 };
-
 form.submitBtn.addEventListener("click", (e) => {
-    // stop reload
-    e.preventDefault();
-    // Opening request
-    const request = window.XMLHttpRequest ?
-      new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-    // Setting request data
-    const requestData = `name=${form.name.value}&email=${form.email.value}&message=${form.message.value}`;
-    // send url
-    const url = 'https://www.aaeqtr.com/wp-content/themes/aae_qtr/contact-form.php';
-    request.open('POST', url, true);
+  // stop reload
+  e.preventDefault();
+  // Opening request
+  const request = window.XMLHttpRequest ?
+    new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+  // Setting request data
+  const requestData = `name=${form.name.value}&email=${form.email.value}&message=${form.message.value}`;
+  // send url
+  const url = 'https://www.aaeqtr.com/wp-content/themes/aae_qtr/contact-form.php';
+  request.open('POST', url, true);
 
-    request.onload = function () {
-      if (this.status == 200 && this.readyState == 4) {
-        form.formMassages.innerHTML = this.responseText;
-      } else {
-        form.formMassages.innerHTML = '<p class="form-massages info">Sending...</p>';
-      }
-    };
+  request.onload = function () {
+    if (this.status == 200 && this.readyState == 4) {
+      form.formMassages.innerHTML = this.responseText;
+    } else {
+      form.formMassages.innerHTML = '<p class="form-massages info">Sending...</p>';
+    }
+  };
 
-    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    request.send(requestData);
-  });
+  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  request.send(requestData);
+});
+
+// animation onload
+window.onload = () => {
+  const heroProject1 = document.querySelector('.projects-preview a:first-child')
+  const heroProject2 = document.querySelector('.projects-preview a:nth-child(2)')
+  const heroProject3 = document.querySelector('.projects-preview a:last-child')
+  // position
+  heroProject1.style.right = heroProject3.style.right = "0"
+  heroProject2.style.left = "0"
+}
